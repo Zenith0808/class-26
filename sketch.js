559bd8b00f07bb0e53cbf3c1c59dec8a6d4fda8a
Zenith0808 <zenithgothi08@gmail.com>
2020-12-29 17:43:47 +0530

    dont run

--- sketch.js
+++ sketch.js
@@ -14,6 +14,7 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
+    
 
 
     ground = new Ground(600,height,1200,20);
@@ -21,6 +22,7 @@ function setup(){
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
+    box9 = new Box(650,100,20,50);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
 
@@ -51,6 +53,7 @@ function draw(){
     log1.display();
 
     box3.display();
+    box9.display();
     box4.display();
     pig3.display();
     log3.display();
